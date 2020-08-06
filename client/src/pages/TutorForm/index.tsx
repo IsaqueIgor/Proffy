c8afffb3acca8c7ed665f55bac46e28d85a65cc6
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-05 23:01:55 -0300

    feat: Tutor Form UI complete

--- client/src/pages/TutorForm/index.tsx
+++ client/src/pages/TutorForm/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PageHeader, Input, TextArea } from '../../components';
+import { PageHeader, Input, TextArea, Select } from '../../components';
 
 import './styles.css';
 
@@ -20,6 +20,48 @@ function TutorForm() {
           <Input name='avatar' label='Avatar (URL)' />
           <Input name='whatsapp' label='Whatsapp' />
           <TextArea name='bio' label='About you' />
+        </fieldset>
+
+        <fieldset>
+          <legend>About the Class</legend>
+          <Select
+            name='subject'
+            label='Subject'
+            options={[
+              { value: 'Arts', label: 'Arts' },
+              { value: 'Physics', label: 'Physics' },
+              { value: 'German', label: 'German' },
+              { value: 'Math', label: 'Math' },
+              { value: 'English', label: 'English' },
+              { value: 'Programmation', label: 'Programmation' },
+            ]}
+          />
+          <Input name='cost' label='How much will cost your hour per lesson?' />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Availability
+            <button type='button'>+ New time</button>
+          </legend>
+
+          <div className='schedule-item'>
+            <Select
+              name='week_day'
+              label='Days of the week'
+              options={[
+                { value: '0', label: 'Sunday' },
+                { value: '1', label: 'Monday' },
+                { value: '2', label: 'Tuesday' },
+                { value: '3', label: 'Wednesday' },
+                { value: '4', label: 'Thursday' },
+                { value: '5', label: 'Friday' },
+                { value: '6', label: 'Saturday' },
+              ]}
+            />
+            <Input name='from' label='From' type='time' />
+            <Input name='to' label='To' type='time' />
+          </div>
         </fieldset>
 
         <footer>
